d4173a13e72923ace6cd4021f7421fbc195c8755
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2025-08-29 09:21:28 +0530

    Update fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, roc_curve
@@ -55,4 +54,5 @@ plt.ylabel("True Positive Rate")
 plt.title("ROC Curve - Fraud Detection")
 plt.legend()
 plt.show()
+
 
